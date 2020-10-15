45a0f2650c8e7fb715e50ca6f2e5964c41851391
alexandr <karpov.a1exandr@yandex.ru>
2020-10-15 14:53:29 +0300

    подключил обработчики к БД по ORM

--- handlers.py
+++ handlers.py
@@ -1,37 +1,27 @@
 import operator
 import re
-from datetime import datetime
+import datetime
 
-from generate_ticket import generate_ticket
-from models import Flights
+from pony.orm import count, db_session, select
 
+from chatbot.generate_ticket import generate_ticket
+from chatbot.models import Flights
 
-try:
-    from settings import flights
-except ImportError:
-    settings = None
-    flights = None
-
-    print('Для работы бота нужно:\n'
-          'создать файл settings.py\n'
-          'в settings.py завести переменные: \n'
-          'flights - словарь словарей (структура будет описана позже)')
-    exit()
 
 re_date = re.compile(r"([0-3][0-9])-([0-1][0-9])-(20\d\d)")
 
 re_name = re.compile(r"(\w{2,}) (\w{2,})")
 
 
-def find_city_by_patterns(user_input):
+def find_city_by_patterns(user_input, cities):
     """
     делаем из инпута пользователя шаблон и сравниваем, с нашим списком городов
+    :param cities: список городов
     :param user_input: город введенный пользователем
     :return: подходящий город str или bool
     """
-    cities = [key for key in flights.keys()]
-    slicer = int(len(user_input) // 1.4)
 
+    slicer = int(len(user_input) // 1.4)
     pattern = f'{user_input[:slicer]}'
     re_city_pattern = re.compile(pattern)
 
@@ -42,6 +32,7 @@ def find_city_by_patterns(user_input):
         return False
 
 
+@db_session
 def handle_departure_city(text, context):
     """
     проверяем введенный город в списке доступных и добавляем в контекст
@@ -49,12 +40,19 @@ def handle_departure_city(text, context):
     :param context: для прохождения сценария
     :return: bool
     """
+
+    # SQL
+    # SELECT DISTINCT departure
+    # FROM flights;
+
+    cities_queryset = select((item.departure,) for item in Flights)[:]
+
     text = text.upper()
-    if text in flights.keys():
+    if text in cities_queryset:
         context['departure'] = text
         return True
     else:
-        text = find_city_by_patterns(text)
+        text = find_city_by_patterns(text, cities_queryset)
         if text:
             context['departure'] = text
             return True
@@ -62,6 +60,7 @@ def handle_departure_city(text, context):
             return False
 
 
+@db_session
 def handle_arrival_city(text, context):
     """
     проверяем введенный город в списке доступных и добавляем в контекст
@@ -69,12 +68,20 @@ def handle_arrival_city(text, context):
     :param context: для прохождения сценария
     :return: bool
     """
+
+    # SQL
+    # SELECT DISTINCT arrival
+    # FROM flights
+    # WHERE departure = context['departure'];
+
+    cities_queryset = select((item.arrival,) for item in Flights if item.departure == context['departure'])[:]
+
     text = text.upper()
-    if text in flights[context['departure']].keys():
+    if text in cities_queryset:
         context['arrival'] = text
         return True
     else:
-        text = find_city_by_patterns(text)
+        text = find_city_by_patterns(text, cities_queryset)
         if text:
             context['arrival'] = text
             return True
@@ -95,25 +102,23 @@ def date_format(text):
         return False
 
 
-def nearest_departure_date(dict_path, users_date, count=5):
+def nearest_departure_date(dates, users_date, date_count=5):
     """
-    если введенной пользователем даты нет, то предлагаем count ближайших
-    :param dict_path: пусть в flights
-    :param users_date: введенная пользователем дата
-    :param count: число ближайших дат
-    :return: готовый текст для печати состоящий из count дат
+    генерируем строку из дат, близжайших к user_date и состоящий из date_count элементов
+    :param dates: список дат
+    :param users_date: введенная пользователем дата -> datetime.date
+    :param date_count: число ближайших дат
+    :return: строка с датами
     """
-    dateset_list = [datetime.strptime(date, '%d-%m-%Y') for date in dict_path.keys()]
-    users_date = datetime.strptime(users_date, '%d-%m-%Y')
 
-    count = len(dict_path.keys()) if len(dict_path.keys()) < count else count
+    date_count = len(dates) if len(dates) < date_count else date_count
 
-    dates = sorted(
-        [(date_from_list, abs((date_from_list - users_date)).days) for date_from_list in dateset_list],
-        key=operator.itemgetter(1), reverse=False)[:count]
-
+    dates_sorted = sorted(
+        [(date_from_list, abs((date_from_list - users_date)).days) for date_from_list in dates],
+        key=operator.itemgetter(1), reverse=False)[:date_count]
+    dates_sorted.sort()
     result = '\nБлижайшие даты:\n'
-    for date in dates:
+    for date in dates_sorted:
         result += f'{date[0].strftime("%d-%m-%Y")}\n'
     return result
 
@@ -125,16 +130,45 @@ def handle_flight_date(text, context):
     :param context: для прохождения сценария
     :return: bool
     """
+
+    # SQL
+    # SELECT date
+    # FROM flights
+    # WHERE departure = context['departure']
+    # and arrival = context['arrival']
+    # and date >= CURDATE();
+
+    today = datetime.date.today()
     if not date_format(text):
         return False
-    if text in flights[context['departure']][context['arrival']].keys():
+
+    user_date = date_to_string(text)
+
+    if user_date < today:
+        context['extra_content'] = '\nнельзя улететь в прошлое :/'
+        return False
+
+    date_queryset = select(
+        (item.date,) for item in Flights if
+        item.departure == context['departure']
+        and item.arrival == context['arrival']
+        and item.date >= today)[:]
+
+    if user_date in date_queryset:
         context['date'] = text
         return True
     else:
         # здесь явно костыль, но другого способа не придумал
         context['extra_content'] = nearest_departure_date(
-            dict_path=flights[context['departure']][context['arrival']], users_date=text)
+            dates=date_queryset, users_date=user_date)
         return False
+
+
+def date_to_string(text):
+    """Превращаем строку с дату"""
+    date = datetime.datetime.strptime(text, '%d-%m-%Y')
+    date = datetime.date(year=date.year, month=date.month, day=date.day)
+    return date
 
 
 def handle_flight_number(text, context):
@@ -144,11 +178,28 @@ def handle_flight_number(text, context):
     :param context: для прохождения сценария
     :return: bool
     """
-    if text in flights[context['departure']][context['arrival']][context['date']]:
-        context['flight_number'] = text
-        return True
-    else:
-        return False
+
+    # SQL
+    # SELECT date
+    # FROM flights
+    # WHERE departure = context['departure']
+    # and arrival = context['arrival']
+    # and date = context['date'];
+
+    user_date = date_to_string(context['date'])
+    seats_query_set = select(
+        (item.flight_number,) for item in Flights if item.departure == context['departure']
+        and item.arrival == context['arrival']
+        and item.date == user_date
+    )[:]
+    try:
+        if int(text) in seats_query_set:
+            context['flight_number'] = text
+            return True
+        else:
+            return False
+    except ValueError:
+        pass
 
 
 def handle_seats(text, context):
@@ -233,67 +284,121 @@ def handle_phone(text, context):
 # по аналогии с хэндлерами сделал в сценарии "действие" перед каждым шагом
 # в основном это вывод информации на экран, скорее всего можно реализовать практичнее и проще
 
-def popular_cities(dict_path, count=5):
+def popular_cities(dict_path, city_count=5):
     """
     возвращаем список городов с самым большим числом авиасообщений
     :param dict_path: путь в flights
-    :param count: число городов
+    :param city_count: число городов
     :return: list of tuples (city_name, count_of_flights)
     """
-    if len(dict_path.keys()) < count:
-        count = len(dict_path.keys())
+    if len(dict_path.keys()) < city_count:
+        city_count = len(dict_path.keys())
 
     return sorted(
         [(key, len(dict_path[key])) for key in dict_path.keys()],
-        key=operator.itemgetter(1), reverse=True)[:count]
+        key=operator.itemgetter(1), reverse=True)[:city_count]
 
 
+@db_session
 def action_print_available_departure_cities():
     """
     выводим пользователю достпные города для вылета
     :return: строка с названиями городов -> str
     """
+
+    # SQL:
+    # SELECT departure, count(DISTINCT arrival)
+    # FROM flights
+    # GROUP BY departure
+    # ORDER BY count(DISTINCT arrival) DESC
+    # LIMIT 5;
+
+    queryset = select(
+        (item.departure, count(item.arrival))
+        for item in Flights).order_by(-2)[:5]
     result = ''
-    cities = popular_cities(flights)
-    for city in cities:
+    for city in queryset:
         result += f'{city[0]} - вылеты в {city[1]} городов\n'
     return f"\nБольше всего рейсов из:\n{result}"
 
 
+@db_session
 def action_print_available_arrival_cities(context):
     """
     выводим пользователю достпные города для прилета
     :param context: ввод от пользователя
     :return: строка с названиями городов -> str
     """
+
+    # SQL:
+    # SELECT arrival, count(arrival)
+    # FROM flights
+    # WHERE departure = '{context['departure']}'
+    # GROUP BY arrival
+    # ORDER BY count(arrival)
+    # DESC LIMIT 5;
+
+    query = select((item.arrival, count(item))
+                   for item in Flights
+                   if item.departure == context['departure']).order_by(-2)[:3]
     result = ''
-    cities = popular_cities(flights[context['departure']], count=3)
-    for city in cities:
+    for city in query:
         result += f'В {city[0]} - {city[1]} рейсов\n'
     return f"\nиз {context['departure']} больше всего рейсов в :\n{result}"
 
 
+@db_session
 def action_print_available_dates(context):
     """
     выводим пользователю 5 ближайших дат
     :param context: ввод от пользователя
     :return: список дат -> str
     """
+
+    # SQL:
+    # SELECT date
+    # FROM flights
+    # WHERE departure = 'context['departure']'
+    # AND arrival = context['arrival]
+    # ORDER BY date DESC
+    # LIMIT 5;
+
+    dates_query = select((item.date,)
+                         for item in Flights
+                         if item.departure == context['departure']
+                         and item.arrival == context['arrival'])[:5]
+    dates_query.sort()
     result = ''
-    for date in flights[context['departure']][context['arrival']]:
+    for date in dates_query:
+        date = date.strftime('%d-%m-%Y')
         result += date + '\n'
     return f'\nДоступные даты:\n{result}'
 
 
+@db_session
 def action_print_available_air_flights(context):
     """
      выводим пользователю 5 рейсов на выбранную дату
     :param context: ввод от пользователя
     :return: список рейсов -> str
     """
+
+    # SQL
+    # SELECT date
+    # FROM flights
+    # WHERE departure = context['departure']
+    # and arrival = context['arrival']
+    # and date = context['date'];
+
+    user_date = date_to_string(context['date'])
+    query_flight_numbers = select((item.flight_number,) for item in Flights
+                                  if item.departure == context['departure']
+                                  and item.arrival == context['arrival']
+                                  and item.date == user_date)[:5]
+
     result = ''
-    for air_flight in flights[context['departure']][context['arrival']][context['date']]:
-        result += air_flight + '\n'
+    for air_flight in query_flight_numbers:
+        result += str(air_flight) + '\n'
     return f'\nДоступные рейсы:\n{result}'
 
 
@@ -341,10 +446,11 @@ def generate_ticket_handler(text, context):
             departure=context['departure'],
             arrival=context['arrival'],
             date=context['date'],
-            email='sample@mail.com'
+            email='sample@mail.com',
+            flight_number=context['flight_number'],
         )
     except KeyError:
-        print('что не так с контекстом')
+        print('что не так с контекстом', text)
 
 
 if __name__ == "__main__":
